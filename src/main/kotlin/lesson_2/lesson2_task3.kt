db4247429523b1f69fae0bf2e540c81a03a9fd80
julxpo <j.poyasnyuk@yandex.ru>
2026-02-18 17:27:22 +0300

    KS-2-3 corrected 3 task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -8,16 +8,19 @@ fun main() {
     – Напиши программу, которая обработает данные и подсчитает час и минуту прибытия поезда;
     – Выведи результат в консоль в формате [часы:минуты], используя двузначное представление чисел (например, 17:05, а не 17:5).*/
 
+    val separator = ":"
+    val timeTransfer = 60
     val hoursDeparture = 9
     val minutesDeparture = 39
-    println("Время отправления: "+hoursDeparture+":"+minutesDeparture)
+    println("Время отправления: 0"+hoursDeparture+separator+minutesDeparture)
 
-    val hoursOnTheWay = 457 / 60
-    val minutesOnTheWay = 457 % 60
     val onTheWay = 457
+    val hoursOnTheWay = onTheWay / timeTransfer
+    val minutesOnTheWay = onTheWay % timeTransfer
     println("В пути "+hoursOnTheWay+" часов "+minutesOnTheWay+" минут")
 
-    val hoursArrival = (onTheWay + minutesDeparture) / 60 + hoursDeparture
-    val minutesArrival = (onTheWay + minutesDeparture) % 60
-    println("Время прибытия: "+hoursArrival+":"+minutesArrival)
+    val minutesInTotal = onTheWay + minutesDeparture
+    val hoursArrival = minutesInTotal / timeTransfer + hoursDeparture
+    val minutesArrival = minutesInTotal % timeTransfer
+    println("Время прибытия: "+hoursArrival+separator+minutesArrival) //про форматирование вывода не поняла, вроде и так правильно же выводится
 }
