e07002981a381000937f5225d245fc253368927c
julxpo <j.poyasnyuk@yandex.ru>
2026-02-19 16:31:57 +0300

    KS-2-3 corrected 3 task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,4 +1,5 @@
 package org.example.lesson_2
+const val TIME_TRANSFER = 60
 
 fun main() {
     /*Сайт с расписанием поездов получает данные с сервера. Сервер посылает время выезда и время в пути,
@@ -8,19 +9,17 @@ fun main() {
     – Напиши программу, которая обработает данные и подсчитает час и минуту прибытия поезда;
     – Выведи результат в консоль в формате [часы:минуты], используя двузначное представление чисел (например, 17:05, а не 17:5).*/
 
-    val separator = ":"
-    val timeTransfer = 60
     val hoursDeparture = 9
     val minutesDeparture = 39
-    println("Время отправления: 0$hoursDeparture$separator$minutesDeparture")
+    println("Время отправления: ${String.format("%02d", hoursDeparture)}:$minutesDeparture")
 
     val onTheWay = 457
-    val hoursOnTheWay = onTheWay / timeTransfer
-    val minutesOnTheWay = onTheWay % timeTransfer
+    val hoursOnTheWay = onTheWay / TIME_TRANSFER
+    val minutesOnTheWay = onTheWay % TIME_TRANSFER
     println("В пути $hoursOnTheWay часов $minutesOnTheWay минут")
 
     val minutesInTotal = onTheWay + minutesDeparture
-    val hoursArrival = minutesInTotal / timeTransfer + hoursDeparture
-    val minutesArrival = minutesInTotal % timeTransfer
-    println("Время прибытия: $hoursArrival$separator$minutesArrival")
+    val hoursArrival = minutesInTotal / TIME_TRANSFER + hoursDeparture
+    val minutesArrival = minutesInTotal % TIME_TRANSFER
+    println("Время прибытия: ${String.format("%2d",hoursArrival)}:$minutesArrival")
 }
